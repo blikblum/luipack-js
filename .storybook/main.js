c6fdfce9ca71b1e6b97f1868f28edb4ca8992d06
Luiz Américo Pereira Câmara <camara_luiz@yahoo.com.br>
2021-12-11 06:51:03 -0300

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
-
 const sassRule = {
   test: /\.(sass|scss)$/,
   use: [
@@ -19,10 +18,11 @@ const sassRule = {
 }
 
 module.exports = {
+  stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-essentials'],
   webpackFinal: async function (config) {
     if (config.name !== 'manager') {
-      // remove web-components babel rule 
+      // remove web-components babel rule
       const ruleIndex = config.module.rules.findIndex(
         (rule) => rule.use && rule.use.options && rule.use.options.babelrc === false
       )
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,33 +1,7 @@
-/* global window */
-
-import {
-  configure,
-  addParameters,
-  // setCustomElements,
-} from '@storybook/web-components'
-
-
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
-// import customElements from '../custom-elements.json';
-
-// setCustomElements(customElements);
-
-addParameters({
+export const parameters = {
   viewport: {
     // defaultViewport: 'tablet',
   },
-})
-
-// configure(require.context('../stories', true, /\.stories\.(js|mdx)$/), module);
-
-// force full reload to not reregister web components
-const req = require.context('../stories', true, /\.stories\.(js|mdx)$/)
-configure(req, module)
-if (module.hot) {
-  module.hot.accept(req.id, () => {
-    const currentLocationHref = window.location.href
-    window.history.pushState(null, null, currentLocationHref)
-    window.location.reload()
-  })
 }
